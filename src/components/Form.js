67425a1b731e972323e7a7114c3737927505e01b
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-08-17 21:02:48 +0530

    text read added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function Form() {
-  const [text, setText] = useState("Enter Text Here");
+  const [text, setText] = useState("");
 
   const handleOnChange = (e) => {
       console.log(e.target.value)
@@ -16,12 +16,22 @@ export default function Form() {
         <textarea className="form-control" value={text} onChange={handleOnChange} id="texareaBox" rows="3"></textarea>
       </div>
       <div className="col-12">
-        <button type="submit" className="btn btn-primary" onClick={() => setText(text.toUpperCase())}>
-          Change To UpperCase
+        <button type="submit" className="btn btn-primary m-2" onClick={() => setText(text.toUpperCase())}>
+          UPPERCASE
         </button>
         <button type="submit" className="btn btn-secondary m-2" onClick={() => setText(text.toLowerCase())}>
-          Change To UpperCase
+            lowercase
         </button>
+        <button type="submit" className="btn btn-secondary m-2" onClick={() => setText('')}>
+          Clear
+        </button>
+      </div>
+
+      <div> 
+        <h1>Summary</h1>
+        <p>{text.split(' ').length} words and {text.length} characters</p>
+        <p>{Math.floor(0.008 * text.split(' ').length)} Minutes read.</p>
+        <p>{text}</p>
       </div>
     </>
   );
