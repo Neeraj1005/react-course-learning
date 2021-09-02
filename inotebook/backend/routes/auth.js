0edcf839b776516a2fde5d960df241602fbd9f19
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-09-02 20:41:49 +0530

    Adding Data Validation Using express-validator

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+
 const router = express.Router();
 
 // create a user using: POST "api/auth". Doesn't require AUTH
-router.post("/", (req, res) => {
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("name")
+      .isLength({ min: 4 })
+      .withMessage("must be at least 4 chars long"),
+    body("email").isEmail(),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        res.json({
+          error: 'please enter unique values for email',
+          message: err.message
+        });
+      });
+  }
+);
 
 module.exports = router;
