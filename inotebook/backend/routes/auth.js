a152e2ccb6b65e9cd1df78c017dc9225b69ced7f
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-09-05 11:43:07 +0530

    Creating Login Endpoint & sending auth token

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -4,11 +4,11 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = '#learnreact';
+const JWT_SECRET = "#learnreact";
 
 const router = express.Router();
 
-// create a user using: POST "api/auth". Register User
+// create a user using: POST "api/auth/register". Register User
 router.post(
   "/register",
   [
@@ -61,7 +61,62 @@ router.post(
       });
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("Error Occured");
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// Login a user using: POST "api/auth/login". Login User
+router.post(
+  "/login",
+  [
+    body("email").isEmail(),
+    body("password")
+      .exists()
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+  ],
+  async (req, res) => {
+    // validation error check
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({email});
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+
+      if (!passwordCompare) {
+        return res.json({
+          error: "please try to login with correct credentials",
+        });
+      }
+
+      // if above all true send token
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({
+        status: 200,
+        token: authtoken,
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server Error");
     }
   }
 );
