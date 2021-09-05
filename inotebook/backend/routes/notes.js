e9eac2a8cbf877d4a73a27c13ffdbd4688207c96
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-09-05 13:06:23 +0530

    Fetching all notes & Adding a Note

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -1,9 +1,59 @@
-const express = require('express')
+const express = require("express");
+const fetchUser = require("../middleware/fetchuser");
 const router = express.Router();
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => {
+// GET: get all the notes "/api/notes/fetchallnotes". Login Required
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  const notes = await Note.find({ user: req.user.id });
+  res.json(notes);
+});
 
-    res.json([])
-})
+// POST: store notes "/api/notes/store". Login Required
+router.post(
+  "/store",
+  fetchUser,
+  [
+    body("title")
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+    body("description")
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+  ],
+  async (req, res) => {
+    try {
+      // destructuring the objec
+      const { title, description, tag } = req.body;
 
-module.exports = router
+      // validation error check
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      // Notes object create
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      // save note to database
+      const savedNote = await note.save();
+
+      // return success response
+      res.json({
+        status: 200,
+        data: savedNote,
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
