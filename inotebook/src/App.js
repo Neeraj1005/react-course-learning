640e5e730016b69d9e4a4dba240618cbd6a76154
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-09-05 17:54:37 +0530

    Creating Navbar and Routes

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
-    <div className="App">
-      This is my iNotebook app
-    </div>
+    <>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ inotebook/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            THis is hoem
+        </div>
+    )
+}
+
+export default Home
