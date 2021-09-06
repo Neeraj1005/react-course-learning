a33f226b1285fdef1d6bc33ff6ccbae956dc000d
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-09-06 20:47:13 +0530

    useLocation Hook in React or template desing

--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -1,24 +1,8 @@
-import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Nick",
-    age: "25",
-  };
-
-  const [state, setState] = useState(s1);
-
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Larry",
-        age: "26",
-      });
-    }, 1000);
-  };
   return (
-    <NoteContext.Provider value={{ state, update }}>
+    <NoteContext.Provider value={{  }}>
       {props.children}
     </NoteContext.Provider>
   );
